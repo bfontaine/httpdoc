508daacafb801d1c528853c9dd97ad8a80e002ef
Baptiste Fontaine <batifon@yahoo.fr>
2015-04-17 23:11:36 +0200

    -root-dir flag added

--- main.go
+++ main.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
 	"github.com/bfontaine/httpdoc/httpdoc"
 )
 
+func printUsage() {
+	fmt.Fprintf(os.Stderr, "Usage:\n    %s [<options>] <code>\n\n", os.Args[0])
+	fmt.Fprintln(os.Stderr, "Where <options> are:")
+	flag.PrintDefaults()
+}
+
 func main() {
+	var doc httpdoc.Doc
+
+	flag.StringVar(&doc.RootDir, "root-dir", "./_docs",
+		"Documentation root directory")
+	flag.Parse()
+
 	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s <code>\n", os.Args[0])
+		printUsage()
 		os.Exit(1)
 	}
-
-	doc := httpdoc.Doc{RootDir: "./_docs"}
 
 	if code, err := doc.GetStatusCode(os.Args[1]); err != nil {
 		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
